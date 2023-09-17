3cc575e45a815437decefd9930b14daf30cb58ab
Maksym Vdovenko <jackjackjack1376@gmail.com>
2023-09-17 12:52:52 +0900

    My Java Snake game.

---
+++ src/GamePanel.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public  void drawMethod (Graphics g){
+
+    }
+    public void move (){
+
+    }
+    public void checkApple(){
+
+    }
+    public  void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public  class  MyKeyAdapter extends KeyAdapter{
+        public void  keyPressed(KeyEvent e){
+
+        }
+    }
+}
